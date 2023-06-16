7bb2786633a37c8062a42f8444290dbc05816276
blackreaper228 <ntsvetnikov@gmail.com>
2023-06-16 03:19:19 +0300

    Билд Разбор Монстров

--- docs/dictionary.js
+++ docs/dictionary.js
@@ -3,5 +3,10 @@
 var __webpack_exports__ = {};
 
 console.log('Тестикс');
+var container = document.querySelector('.C_MonsterCards');
+container.addEventListener('wheel', function (e) {
+  e.preventDefault();
+  container.scrollLeft += e.deltaY;
+});
 /******/ })()
 ;
