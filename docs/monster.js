243618b5b5a8400968dfa9c5466c5f0125d3ee10
blackreaper228 <ntsvetnikov@gmail.com>
2023-06-21 17:48:11 +0300

    Настраивал классы body

--- docs/monster.js
+++ docs/monster.js
@@ -1,11 +1,17 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
+ // Горизонтальный скролл
 
 var container = document.querySelector('.C_MonsterCards');
 container.addEventListener('wheel', function (e) {
   e.preventDefault();
   container.scrollLeft += e.deltaY;
+}); // Клик по карточкам
+
+var card = document.getElementById('card_01');
+card.addEventListener('click', function (e) {
+  console.log('click');
 });
 /******/ })()
 ;
--- src/monster.js
+++ src/monster.js
@@ -1,8 +1,18 @@
 import './dictionary.css'
 
+// Горизонтальный скролл
+
 const container = document.querySelector('.C_MonsterCards')
 
 container.addEventListener('wheel', (e) => {
   e.preventDefault()
   container.scrollLeft += e.deltaY
+})
+
+// Клик по карточкам
+
+const card = document.getElementById('card_01')
+
+card.addEventListener('click', (e) => {
+  console.log('click')
 })
