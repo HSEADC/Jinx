1a9c8c969b61103b8a712e0904f8abd802212d9b
blackreaper228 <ntsvetnikov@gmail.com>
2023-06-22 05:23:14 +0300

    Вставил контент для разбора сюжета

--- docs/story.js
+++ docs/story.js
@@ -40,7 +40,172 @@ var screenmobile = window.matchMedia('(max-device-width: 462px)');
 if (screenmobile.matches) {
   cards.scrollTop = 77;
 } // Клик по карточкам
-// const card01 = document.getElementById('card_01')
-// let article01 = document.getElementById('article_01')
+
+
+var card01 = document.getElementById('story_card_01');
+var card02 = document.getElementById('story_card_02');
+var card03 = document.getElementById('story_card_03');
+var card04 = document.getElementById('story_card_04');
+var card05 = document.getElementById('story_card_05');
+var card06 = document.getElementById('story_card_06');
+var card07 = document.getElementById('story_card_07');
+var card08 = document.getElementById('story_card_08');
+var card09 = document.getElementById('story_card_09');
+var card10 = document.getElementById('story_card_10');
+var card11 = document.getElementById('story_card_11');
+var article01 = document.getElementById('article_01');
+var article02 = document.getElementById('article_02');
+var article03 = document.getElementById('article_03');
+var article04 = document.getElementById('article_04');
+var article05 = document.getElementById('article_05');
+var article06 = document.getElementById('article_06');
+var article07 = document.getElementById('article_07');
+var article08 = document.getElementById('article_08');
+var article09 = document.getElementById('article_09');
+var article10 = document.getElementById('article_10');
+var article11 = document.getElementById('article_11');
+card01.addEventListener('click', function (e) {
+  article01.style.display = 'block';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card02.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'block';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card03.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'block';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card04.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'block';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card05.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'block';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card06.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'block';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card07.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'block';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card08.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'block';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card09.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'block';
+  article10.style.display = 'none';
+  article11.style.display = 'none';
+});
+card10.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'block';
+  article11.style.display = 'none';
+});
+card11.addEventListener('click', function (e) {
+  article01.style.display = 'none';
+  article02.style.display = 'none';
+  article03.style.display = 'none';
+  article04.style.display = 'none';
+  article05.style.display = 'none';
+  article06.style.display = 'none';
+  article07.style.display = 'none';
+  article08.style.display = 'none';
+  article09.style.display = 'none';
+  article10.style.display = 'none';
+  article11.style.display = 'block';
+});
 /******/ })()
 ;
--- src/story.js
+++ src/story.js
@@ -53,6 +53,181 @@ if (screenmobile.matches) {
 
 // Клик по карточкам
 
-// const card01 = document.getElementById('card_01')
+const card01 = document.getElementById('story_card_01')
+const card02 = document.getElementById('story_card_02')
+const card03 = document.getElementById('story_card_03')
 
-// let article01 = document.getElementById('article_01')
+const card04 = document.getElementById('story_card_04')
+const card05 = document.getElementById('story_card_05')
+const card06 = document.getElementById('story_card_06')
+const card07 = document.getElementById('story_card_07')
+const card08 = document.getElementById('story_card_08')
+const card09 = document.getElementById('story_card_09')
+const card10 = document.getElementById('story_card_10')
+const card11 = document.getElementById('story_card_11')
+
+let article01 = document.getElementById('article_01')
+let article02 = document.getElementById('article_02')
+let article03 = document.getElementById('article_03')
+let article04 = document.getElementById('article_04')
+let article05 = document.getElementById('article_05')
+let article06 = document.getElementById('article_06')
+let article07 = document.getElementById('article_07')
+let article08 = document.getElementById('article_08')
+let article09 = document.getElementById('article_09')
+let article10 = document.getElementById('article_10')
+let article11 = document.getElementById('article_11')
+
+card01.addEventListener('click', (e) => {
+  article01.style.display = 'block'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card02.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'block'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card03.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'block'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card04.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'block'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card05.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'block'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card06.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'block'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card07.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'block'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card08.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'block'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card09.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'block'
+  article10.style.display = 'none'
+  article11.style.display = 'none'
+})
+
+card10.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'block'
+  article11.style.display = 'none'
+})
+
+card11.addEventListener('click', (e) => {
+  article01.style.display = 'none'
+  article02.style.display = 'none'
+  article03.style.display = 'none'
+  article04.style.display = 'none'
+  article05.style.display = 'none'
+  article06.style.display = 'none'
+  article07.style.display = 'none'
+  article08.style.display = 'none'
+  article09.style.display = 'none'
+  article10.style.display = 'none'
+  article11.style.display = 'block'
+})
